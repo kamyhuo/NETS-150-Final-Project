3285330ec429d6f7aa976e71b7f43505dc8a8907
Varun Ramakrishnan <vramakrishnan1999@gmail.com>
2019-04-24 11:08:42 -0400

    added frontend server test

--- client/src/App.js
+++ client/src/App.js
@@ -102,6 +102,35 @@ class App extends Component {
   this.setState({ done: true, showing: false });
 }
 
+state = {
+  response: '',
+  post: '',
+  responseToPost: '',
+};
+componentDidMount() {
+  this.callApi()
+    .then(res => this.setState({ response: res.express }))
+    .catch(err => console.log(err));
+}
+callApi = async () => {
+  const response = await fetch('/api/hello');
+  const body = await response.json();
+  if (response.status !== 200) throw Error(body.message);
+  return body;
+};
+handleSubmit = async e => {
+  e.preventDefault();
+  const response = await fetch('/api/world', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify({ post: this.state.post }),
+  });
+  const body = await response.text();
+  this.setState({ responseToPost: body });
+};
+
   render() {
     const { showing } = this.state;
     return (
@@ -141,6 +170,19 @@ class App extends Component {
                     
         </header>
 
+        <p>{this.state.response}</p>
+        <form onSubmit={this.handleSubmit}>
+          <p>
+            <strong>Post to Server:</strong>
+          </p>
+          <input
+            type="text"
+            value={this.state.post}
+            onChange={e => this.setState({ post: e.target.value })}
+          />
+          <button type="submit">Submit</button>
+        </form>
+        <p>{this.state.responseToPost}</p>
       </div>
 
     );
